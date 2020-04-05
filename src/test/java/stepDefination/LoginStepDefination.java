58ecde5f029aac231cd2dcf4da5c82ea68b00fb7
Lipsita <lipsita.987@gmail.com>
2020-04-05 18:28:45 +0530

    Editted .gitignorefile

--- src/test/java/stepDefination/LoginStepDefination.java
+++ src/test/java/stepDefination/LoginStepDefination.java
@@ -1,13 +1,11 @@
 package stepDefination;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
